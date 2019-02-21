5b566f1705ed31ae0fd98085558d5efd61e9bb40
Emma Andrews <eandrews258@gmail.com>
2019-02-21 00:46:14 -0500

    initial

---
+++ asciiart.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from PIL import Image
+import numpy
+import math
+
+ASCII = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+openim = input("Image to load (include file extension): ")
+im = Image.open(openim)
+
+data = numpy.asarray(im)
+
+brightness = numpy.zeros([im.size[0], im.size[1]])
+ascii_matrix = numpy.chararray([im.size[0], im.size[1]], unicode=True)
+
+for x in range(im.size[0] - 1):
+    for y in range(im.size[1] - 1):
+        avg = (int(data[x][y][0]) + int(data[x][y][1]) + int(data[x][y][2])) / 3
+        brightness[x][y] = math.ceil(avg)
+        index = int(math.floor(brightness[x][y] / 4))
+        ascii_matrix[x][y] = ASCII[index]
+        print(ascii_matrix[x][y], end='')
+    print()
