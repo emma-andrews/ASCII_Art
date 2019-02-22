032ecb73f79cc53fa65b4df0df104d848d2a422a
Emma Andrews <eandrews258@gmail.com>
2019-02-22 10:58:47 -0500

    fixed error on non-square images

--- asciiart.py
+++ asciiart.py
@@ -9,15 +9,16 @@ openim = input("Image to load (include file extension): ")
 im = Image.open(openim)
 
 data = numpy.asarray(im)
+brightness = numpy.zeros([im.size[1], im.size[0]])
+ascii_matrix = numpy.chararray([im.size[1], im.size[0]], unicode=True)
 
-brightness = numpy.zeros([im.size[0], im.size[1]])
-ascii_matrix = numpy.chararray([im.size[0], im.size[1]], unicode=True)
-
-for x in range(im.size[0] - 1):
-    for y in range(im.size[1] - 1):
+for x in range(im.size[1] - 1):
+    for y in range(im.size[0] - 1):
         avg = (int(data[x][y][0]) + int(data[x][y][1]) + int(data[x][y][2])) / 3
         brightness[x][y] = math.ceil(avg)
         index = int(math.floor(brightness[x][y] / 4))
+        if index > 67:
+            index = 67
         ascii_matrix[x][y] = ASCII[index]
         print(ascii_matrix[x][y], end='')
     print()
