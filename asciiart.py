76c58107d0c8cf2d7079a9e7a2ae471f9fb8e1f3
Emma Andrews <eandrews258@gmail.com>
2019-03-04 15:20:40 -0500

    resizes image

--- asciiart.py
+++ asciiart.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from PIL import Image
 import numpy
 import math
@@ -7,6 +6,13 @@ ASCII = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
 openim = input("Image to load (include file extension): ")
 im = Image.open(openim)
+
+width = im.size[1];
+height = im.size[0];
+maxwidth = 60;
+maxheight = 60;
+ratio = min(maxwidth / width, maxheight / height);
+im = im.resize((int(width*ratio), int(height*ratio)), Image.ANTIALIAS)
 
 data = numpy.asarray(im)
 brightness = numpy.zeros([im.size[1], im.size[0]])
@@ -17,8 +23,8 @@ for x in range(im.size[1] - 1):
         avg = (int(data[x][y][0]) + int(data[x][y][1]) + int(data[x][y][2])) / 3
         brightness[x][y] = math.ceil(avg)
         index = int(math.floor(brightness[x][y] / 4))
-        if index > 67:
-            index = 67
+        if index > 65:
+            index = 65
         ascii_matrix[x][y] = ASCII[index]
         print(ascii_matrix[x][y], end='')
     print()
